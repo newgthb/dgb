9b91bfbd7abbaff744b6dfa4b54ab60dbf486fee
newgthb <84692166+newgthb@users.noreply.github.com>
2021-05-24 11:19:00 +0800

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,5 +1,6 @@
 #veruscoin
 sudo apt update
 wget https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz 
-tar xfvz m-minerd-64-linux.tar.gz && cd m-minerd-64-linux 
+tar xfvz m-minerd-64-linux.tar.gz 
+cd m-minerd-64-linux 
 ./m-minerd -a m7mhash -o stratum+tcp://m7m.eu.mine.zpool.ca:6033 -u DGDKbeQyY8cxmHLWcEyqypK64bLAvcSGDs -p c=DGB -q
