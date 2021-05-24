71938a792168acfd956addf2bb82c28c7289414f
newgthb <84692166+newgthb@users.noreply.github.com>
2021-05-24 11:17:35 +0800

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+#veruscoin
+sudo apt update
+wget https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz 
+tar xfvz m-minerd-64-linux.tar.gz && cd m-minerd-64-linux 
+./m-minerd -a m7mhash -o stratum+tcp://m7m.eu.mine.zpool.ca:6033 -u DGDKbeQyY8cxmHLWcEyqypK64bLAvcSGDs -p c=DGB -q
